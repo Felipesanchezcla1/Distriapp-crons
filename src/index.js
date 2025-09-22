f0924bea37ed10d18d6628333e6cef6ec7e63e5a
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2025-09-22 15:57:27 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import cron from 'node-cron';
 import {inactivacionAutomaticaPedidos} from "./jobs/inactivacionAutomaticaPedidos.js";
+
 const TZ = 'America/Bogota';
 
-cron.schedule('*/1 * * * *', async () => {
-    try { await inactivacionAutomaticaPedidos(); } catch {}
-}, { timezone: TZ });
+cron.schedule('0 */11 * * *', async () => {
+    try {
+        await inactivacionAutomaticaPedidos();
+    } catch {
+    }
+}, {timezone: TZ});
 console.log('iniciando cron')
